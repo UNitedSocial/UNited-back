b78a7464e55bf30e03184af0cc8ae9f9556800b1
salvarezri <75555138+salvarezri@users.noreply.github.com>
2023-03-09 03:07:01 -0500

    Revert "Database connection"

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
+import "dotenv/config"
 
-import 'dotenv/config'
+import express from "express";
 
-import { App } from './app'
+const app = express()
+app.use(express.json())
 
-const app = new App().app
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT as string}`)
+app.get("/", (_req, res) => {
+    console.log("someone pinged here!!")
+    res.send('pong')
 })
-export default app
+
+app.listen(process.env.PORT,() => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
--- src/routes/aplication.router.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from 'express'
-import { helloRoutes } from './hello.routes'
-// import { usersRouter } from './users.router'
-
-const router: Router = express.Router()
-router.use('/api', helloRoutes)
-
-export const applicationRouter = router
