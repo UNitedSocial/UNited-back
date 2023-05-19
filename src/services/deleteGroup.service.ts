1118151a18169f52be040d2212c3248e1b067941
Efrain <dmojicam@unal.edu.co>
2023-05-19 01:09:46 -0500

    fix delete groups function

--- src/services/deleteGroup.service.ts
+++ src/services/deleteGroup.service.ts
@@ -1,11 +1,14 @@
 import GroupModel from '../models/Group.model'
 import { GroupDocument } from '../models/group.documents'
+import UserModel from '../models/User.model'
+import { UserDocument } from '../models/user.documents'
 import { Responses, ResponseStatus } from '../types/response.types'
-
+import requestsServices from '../services/requests.service'
 class DeleteGroup {
   public async deleteGroup (groupname: string): Promise<Responses> {
     let response: Responses
     let group: GroupDocument | null = null
+    let user: UserDocument [] = []
 
     // Get group
     try {
@@ -18,15 +21,51 @@ class DeleteGroup {
       return response
     }
 
-    // Check if group exists
-    if (group === null) {
+    // Get group
+    try {
+      user = await UserModel.find({ 'groups.groupName': groupname }, { _id: 0, __v: 0 })
+    } catch {
+      response = {
+        status: ResponseStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error finding user'
+      }
+      return response
+    }
+    // Check if group and user exists
+    if (group === null || user === null) {
       response = {
         status: ResponseStatus.NOT_FOUND,
-        message: 'The Group doesn\'t exist'
+        message: 'The Group or User doesn\'t exist'
       }
       return response
     }
 
+    // Delete the group from the user
+    try {
+      for (let i = 0; i < user.length; i++) {
+        if (!requestsServices.userIsInGroup(user[i], group)) {
+          response = {
+            status: ResponseStatus.BAD_REQUEST,
+            message: 'User isn\'t in the group'
+          }
+          return response
+        }
+        user[i]?.groups.forEach((group) => {
+          if (group.groupName === groupname) {
+            user[i]?.groups.splice(user[i]?.groups.indexOf(group), 1)
+          }
+        })
+
+        const usergroups = user[i].groups
+        await UserModel.updateOne({ 'groups.groupName': groupname }, { groups: usergroups })
+      }
+    } catch {
+      response = {
+        status: ResponseStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error deleting group from user'
+      }
+      return response
+    }
     // Delete the group
     try {
       await GroupModel.deleteOne({ 'info.name': groupname })
